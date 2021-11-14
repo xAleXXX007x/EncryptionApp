782cc677fa52336879a2b9288717477e61d761de
AleXXX_007 <AleXXX_007@inbox.ru>
2021-11-14 15:22:13 +0400

    refactor

--- main.py
+++ main.py
@@ -46,36 +46,23 @@ def bitcodeFromBlocks(blocks):
   return output
 
 def initialPermutation(block):
-  output = ""
-  pattern = [
+  return toPattern(block, [
     58, 50, 42, 34, 26, 18, 10, 2, 60, 52, 44, 36, 28, 20, 12, 4,
     62, 54, 46, 38, 30, 22, 14, 6, 64, 56, 48, 40, 32, 24, 16, 8,
     57, 49, 41, 33, 25, 17, 9,  1, 59, 51, 43, 35, 27, 19, 11, 3,
     61, 53, 45, 37, 29, 21, 13, 5, 63, 55, 47, 39, 31, 23, 15, 7
-  ]
-
-  for i in pattern:
-    output += block[i - 1]
-  
-  return output
+  ])
 
 def endingPermutation(block):
-  output = ""
-  pattern = [
+  return toPattern(block, [
     40, 8, 48, 16, 56, 24, 64, 32, 39, 7, 47, 15, 55, 23, 63, 31,
     38, 6, 46, 14, 54, 22, 62, 30, 37, 5, 45, 13, 53, 21, 61, 29,
     36, 4, 44, 12, 52, 20, 60, 28, 35, 3, 43, 11, 51, 19, 59, 27,
     34, 2, 42, 10, 50, 18, 58, 26, 33, 1, 41, 9,  49, 17, 57, 25
-  ]
-
-  for i in pattern:
-    output += block[i - 1]
-  
-  return output
+  ])
 
 def E(block):
-  output = ""
-  pattern = [
+  return toPattern(block, [
     32, 1,  2,  3,  4,  5,
     4,  5,  6,  7,  8,  9,
     8,  9,  10, 11, 12, 13,
@@ -84,12 +71,7 @@ def E(block):
     20, 21, 22, 23, 24, 25,
     24, 25, 26, 27, 28, 29,
     28, 29, 30, 31, 32, 1
-  ]
-
-  for i in pattern:
-    output += block[i - 1]
-
-  return output
+  ])
 
 def generateKeys(key):
   if (len(key) != 7):
@@ -108,18 +90,13 @@ def generateKeys(key):
     
     key64 += byte
   
-  pattern = [
+  key56 = toPattern(key64, [
     57, 49, 41, 33, 25, 17, 9,  1,  58, 50, 42, 34, 26, 18,
     10, 2,  59, 51, 43, 35, 27, 19, 11, 3,  60, 52, 44, 36,
     63, 55, 47, 39, 31, 23, 15, 7,  62, 54, 46, 38, 30, 22,
     14, 6,  61, 53, 45, 37, 29, 21, 13, 5,  28, 20, 12, 4
-  ]
+  ])
 
-  key56 = ""
-
-  for i in pattern:
-    key56 += key64[i - 1]
-  
   keys = []
 
   C, D = key56[:28], key56[28:]
@@ -141,12 +118,7 @@ def generateKeys(key):
 
   for keyPair in keys:
     key56 = keyPair[0] + keyPair[1]
-    key = ""
-
-    for i in pattern:
-      key += key56[i - 1]
-
-    output.append(key)
+    output.append(toPattern(key56, pattern))
 
   return output
   
@@ -177,19 +149,12 @@ def F(block, key):
   for i in range(0, 8):
     bits += S(i, bitcode[i * 6:i * 6 + 6])
 
-  pattern = [
+  return toPattern(bits, [
     16, 7,  20, 21, 29, 12, 28, 17,
     1,  15, 23, 26, 5,  18, 31, 10,
     2,  8,  24, 14, 32, 27, 3,  9,
     19, 13, 30, 6,  22, 11, 4,  25
-  ]
-
-  output = ""
-
-  for i in pattern:
-    output += bits[i - 1]
-
-  return output
+  ])
 
 SPatterns = [
   [
